bfa985f8e5e061e8d5c764f4c990f90b4b2a10a3
SHIINASAMA <SHIINA_KAORU@OUTLOOK.COM>
2020-09-14 18:04:51 +0800

    适配器格式化写操作

--- ToolKits/Adapter/ILrcAdapter.cs
+++ ToolKits/Adapter/ILrcAdapter.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using ToolKits.LrcData;
 
-namespace ToolKits
+namespace ToolKits.Adapter
 {
     interface ILrcAdapter
     {
---
+++ ToolKits/Adapter/LrcAdapter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using ToolKits.LrcData;
+
+namespace ToolKits.Adapter
+{
+    public class LrcAdapter : ILrcAdapter
+    {
+        public void ReadLrcFile(ref LrcObject lrcObject, string objectPath)
+        {
+            
+        }
+
+        public void WriteLrcFile(ref LrcObject lrcObject, string objectPath)
+        {
+            using (StreamWriter writer = new StreamWriter(objectPath))
+            {
+                foreach (LrcHeader header in lrcObject.Headers)
+                {
+                    writer.WriteLine(header.FormatLrc() + "\r\n");
+                }
+                foreach (LrcLine line in lrcObject.Lines)
+                {
+                    writer.WriteLine(line.FormatLrc() + "\r\n");
+                }
+                writer.Flush();
+            }
+        }
+    }
+}
